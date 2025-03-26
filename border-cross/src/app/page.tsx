d9c042fcb5c73a8652e6f4577442db2c15fb098a
nulxn <oioioihomelander@yahoo.com>
2025-03-26 11:21:10 -0700

    page

--- border-cross/src/app/page.tsx
+++ border-cross/src/app/page.tsx
@@ -1,3 +1,9 @@
 import Image from "next/image";
 
-export default function Home() {}
+export default function Home() {
+    return (
+        <div>
+            <h1>hello, world!</h1>
+        </div>
+    )
+}
