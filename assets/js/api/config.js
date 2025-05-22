75f3e3453582df551dfb016cc6df99bbba06f1d0
CyberLord09 <anvay.vahia@gmail.com>
2025-05-22 10:37:49 -0700

    Revert "fix port - nolan"
    This reverts commit fd168473add101f1f0d74b0aedaa4fd71dbd8fb5.

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-  pythonURI = "http://localhost:4887";
+  pythonURI = "http://localhost:3167";
 } else if (location.hostname === "127.0.0.1") {
-  pythonURI = "http://127.0.0.1:4887";
+  pythonURI = "http://127.0.0.1:3167";
 } else {
   pythonURI = "https://crosswise.stu.nighthawkcodingsociety.com";
 }
