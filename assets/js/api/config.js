fd168473add101f1f0d74b0aedaa4fd71dbd8fb5
YashPatil321 <yashp.d39@gmail.com>
2025-05-22 10:28:10 -0700

    fix port - nolan

--- assets/js/api/config.js
+++ assets/js/api/config.js
@@ -1,8 +1,8 @@
 export var pythonURI;
 if (location.hostname === "localhost") {
-  pythonURI = "http://localhost:3167";
+  pythonURI = "http://localhost:4887";
 } else if (location.hostname === "127.0.0.1") {
-  pythonURI = "http://127.0.0.1:3167";
+  pythonURI = "http://127.0.0.1:4887";
 } else {
   pythonURI = "https://crosswise.stu.nighthawkcodingsociety.com";
 }
